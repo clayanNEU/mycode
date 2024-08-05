3700c0d469fcbbdc7ccb8314c9af420538a14068
Clayton <clayanbusy@gmail.com>
2024-08-05 19:13:24 +0000

    input

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Research | clayan
+    print() - display data to std out"""
+
+# function
+def main():
+    # pause program for input
+    user_input = input("Please enter IP addy:")
+    print("ur ip addy is: " + user_input)
+
+main()
